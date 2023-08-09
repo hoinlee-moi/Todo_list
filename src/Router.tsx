46a1ee746b976a93a1fedcbf9ea3cb5117138683
hoinlee-moi <lhi3048@gmail.com>
2023-08-09 12:09:21 +0900

    fix : router (routes in Router component, createBrowerRouter)

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,42 +1,37 @@
-import {  RouterProvider, useRoutes } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  useRoutes,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Todo from "./pages/Todo";
 import NoMatch from "./pages/NoMatch";
 
-
-
 // 토큰 검사 함수
 const isAuthenticated = () => {
- // 쿠키에서 토큰 검사 결과 return
-  return  null;
+  // 쿠키에서 토큰 검사 결과 return
+  return null;
 };
 
-const routes = [
-  { path: "/", element: <Home /> },
-  { path: "/signup", element: <Signup /> },
-  { path: "/signin", element: <Signin /> },
-  { path: "/todo", element: <Todo /> },
-  { path: "*", element: <NoMatch /> },
-];
-
 const Router = () => {
-  // useRoutes 훅을 사용하여 라우트를 처리합니다.
-  const element = useRoutes(routes);
-  const pathname = window.location.pathname
+  // 각 라우팅될 페이지를 모아놓습니다
+  const routes = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/signup", element: <Signup /> },
+    { path: "/signin", element: <Signin /> },
+    { path: "/todo", element: <Todo /> },
+    { path: "*", element: <NoMatch /> },
+  ]);
+  const pathname = window.location.pathname;
+  //특정 페이지 접근시 토큰 검사를 실시합니다
 
-  switch(pathname){
-    case "/todo"
-  }
+  // switch(pathname){
+  //   case "/todo"
+  // }
 
-  // 사용자가 로그인하지 않은 경우 "/signin" 페이지로 리다이렉트
-  if (!isAuthenticated()) {
-    // navigate("/signin");
-    return null;
-  }
-
-//   return <RouterProvider router={element} />;
+  return <RouterProvider router={routes} />;
 };
 
 export default Router;
