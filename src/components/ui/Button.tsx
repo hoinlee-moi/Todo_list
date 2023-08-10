9347bf69cd1630b2091e4f977ff002b05078913c
hoinlee-moi <lhi3048@gmail.com>
2023-08-10 19:33:04 +0900

    feat : ui component Button

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,23 @@
+import { Children } from "react";
+
+interface ButtonProps {
+  children: string;
+  style?: string;
+  disabled: boolean;
+  clickHandle: () => void;
+}
+
+const Button = ({
+  children,
+  style,
+  disabled = false,
+  clickHandle,
+}: ButtonProps) => {
+  return (
+    <button className={style} disabled={disabled} onClick={clickHandle}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
