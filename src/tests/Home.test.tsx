470c8ad2eb14138e00524644849070163cbb5bb0
hoinlee-moi <lhi3048@gmail.com>
2023-08-10 22:10:50 +0900

    fix : Home test code fix

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Home from "../pages/Home";
@@ -14,24 +14,25 @@ describe("<Home />", () => {
   });
   //홈페이지 타이틀 메세지가 잘 뜨는지 확인
   test("render home title massage", () => {
-    const HomeTitleMessage = screen.getByRole("h1", {
-      name: "TODOLIST에 오신 걸 환영합니다!",
+    const HomeTitleMessage = screen.getByRole("heading", {
+      name: /TODOLIST에 오신 걸 환영합니다/i,
     });
+
     expect(HomeTitleMessage).toBeInTheDocument();
   });
 
-  //Login 버튼 누를시 signin페이지 이동
-  test("click login button redirect /signin", () => {
+  //로그인 버튼 누를시 signin페이지 이동
+  test("click login button redirect /signin", async () => {
     const loginButton = screen.getByRole("button", { name: "로그인" });
-    fireEvent.click(loginButton);
-    expect(window.location.pathname).toBe("/signin");
+    await fireEvent.click(loginButton);
+    expect(screen.getByText(/로그인/i)).toBeInTheDocument();
+    // expect(window.location.pathname).toBe("/signin");
   });
 
-  //signup버튼 누를시 signup페이지 이동
-  test("click signup button redirect /signup", () => {
+  //회원가입버튼 누를시 signup페이지 이동
+  test("click signup button redirect /signup", async () => {
     const signupButton = screen.getByRole("button", { name: "회원가입" });
-    fireEvent.click(signupButton);
-    expect(window.location.pathname).toBe("signup");
+    await fireEvent.click(signupButton);
+    expect(screen.getByText(/회원가입/i)).toBeInTheDocument();
   });
 });
-
