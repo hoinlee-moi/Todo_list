77c1b40ed271daa854c7da768379ac71c1b7a36c
hoinlee-moi <lhi3048@gmail.com>
2023-08-10 20:41:22 +0900

    feat : home page component, style add

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
-import styles from "../styles/home.module.css"
+import { useNavigate } from "react-router";
+import Button from "../components/ui/Button";
+import styles from "../styles/home.module.css";
 
 const Home = () => {
-    return <div>Home페이지입니다</div>
-}
+  const navigation = useNavigate();
+  const signinBtnRedirectHandle = () => navigation("signin");
+  const signupBtnRedirectHandle = () => navigation("signup");
+  return (
+    <div className={styles.homeContainer}>
+      <section className={styles.titleContainer}>
+        <h1>TODOLIST에 오신 걸 환영합니다!</h1>
+      </section>
+      <section className={styles.redirectBtnContainer}>
+        <Button clickHandle={signinBtnRedirectHandle}>로그인</Button>
+        <Button clickHandle={signupBtnRedirectHandle}>회원가입</Button>
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home!;
