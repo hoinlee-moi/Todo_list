f2691ed27e6a68a8ddd950df334575ba39159053
hoinlee-moi <lhi3048@gmail.com>
2023-08-11 21:12:51 +0900

    feat: signup page func add, validate func add

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,24 +1,41 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import styles from "../styles/signup.module.css";
 import Button from "../components/ui/Button";
 import SignupInput from "../components/signup/SignupInput";
+import { authType } from "../types/authTypes";
+import { validateEmail, validatePassword } from "../util/validation";
 
 const Signup = () => {
   const navigate = useNavigate();
-  const [signupInputVal, setSignupInputVal] = useState({
+  const [signupInputVal, setSignupInputVal] = useState<authType>({
     email: "",
     password: "",
   });
-  const [signupInvalid, setSignInvalid] = useState(true);
-  const [signupLoading, setSignupLoading] = useState(false)
+  const [signupInvalid, setSignInvalid] = useState<boolean>(true);
+  const [signupLoading, setSignupLoading] = useState<boolean>(false);
 
-  const signupInputChangeHandler = () => {};
+  const cancelBtnRedirectHandler = useCallback((): void => navigate("/"), []);
 
-  const cancelBtnRedirectHandler = () => navigate("/");
+  const signupInputChangeHandler = (target: string, value: string): void => {
+    const newValue = newSignupValue(target, value);
+    setSignupInputVal(newValue);
+    validateValue(newValue);
+  };
+
+  const newSignupValue = (target: string, value: string): authType => {
+    return { ...signupInputVal, [target]: value };
+  };
+
+  const validateValue = (value: authType): void => {
+    if (validateEmail(value.email) && validatePassword(value.password))
+      setSignInvalid(false);
+    else setSignInvalid(true);
+  };
+
   const signupBtnHandler = () => {
-    console.log("클릭");
+    console.log(signupInputVal);
   };
 
   return (
