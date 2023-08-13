999a78481ba5fbedb065659200f9d3005b68534c
hoinlee-moi <lhi3048@gmail.com>
2023-08-14 03:10:39 +0900

    feat : dummyTodo create

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -2,7 +2,32 @@ import CompletedList from "../components/todo/CompletedList";
 import IncompletedList from "../components/todo/IncompletedList";
 import TodoCreate from "../components/todo/TodoCreate";
 import styles from "../styles/pages/todo.module.css";
-
+export const DUMMY = [
+  {
+    "id": 1,
+    "todo": "todo2",
+    "isCompleted": false,
+    "userId": 1
+  },
+  {
+    "id": 2,
+    "todo": "todo3",
+    "isCompleted": false,
+    "userId": 1
+  },
+  {
+    "id": 3,
+    "todo": "todo4",
+    "isCompleted": false,
+    "userId": 1
+  },
+  {
+    "id": 4,
+    "todo": "todo5",
+    "isCompleted": true,
+    "userId": 1
+  }
+]
 const Todo = () => {
   return (
     <div className={styles.pageContainer}>
@@ -10,8 +35,8 @@ const Todo = () => {
         <h1 className={styles.pageTitle}>TODO</h1>
         <TodoCreate />
         <section className={styles.listContainer}>
-          <CompletedList />
           <IncompletedList />
+          <CompletedList />
         </section>
       </div>
     </div>
