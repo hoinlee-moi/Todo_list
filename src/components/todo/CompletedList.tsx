89dc4c8c6af8d55156c0d793df22faf3bfe9626f
hoinlee-moi <lhi3048@gmail.com>
2023-08-14 03:10:55 +0900

    feat : todoList article create

--- src/components/todo/CompletedList.tsx
+++ src/components/todo/CompletedList.tsx
@@ -1,10 +1,29 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { DUMMY } from "../../pages/Todo";
 import styles from "../../styles/pages/todo.module.css";
-
+import { faSquareCheck } from "@fortawesome/free-solid-svg-icons";
+const dummy = DUMMY;
 const CompletedList = () => {
   return (
     <article>
-      <h3>완료!</h3>
-      <div className={styles.todoListWrap}></div>
+      <h3>완료</h3>
+      <div className={styles.todoListWrap}>
+        {dummy.map((item) => {
+          return (
+            <li
+              key={item.id}
+              id={item.id.toString()}
+              className={styles.todoItem}
+            >
+              <label>
+                <FontAwesomeIcon icon={faSquareCheck} />
+                <input type="checkbox" defaultChecked={item.isCompleted} />
+                <span>{item.todo}</span>
+              </label>
+            </li>
+          );
+        })}
+      </div>
     </article>
   );
 };
