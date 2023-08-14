83b2e3417c39681f6ec5460aed00da2a710a1849
hoinlee-moi <lhi3048@gmail.com>
2023-08-14 21:53:32 +0900

    feat : list component code add

--- src/components/todo/CompletedList.tsx
+++ src/components/todo/CompletedList.tsx
@@ -1,17 +1,20 @@
-import { DUMMY } from "../../pages/Todo";
+import { useContext } from "react";
 import styles from "../../styles/pages/todo.module.css";
+import { Todo } from "../../types/todoTypes";
 import TodoItem from "./TodoItem";
+import { TodoContext } from "../../contexts/TodoContext";
 
-const CompletedList = () => {
-  const dummy = DUMMY;
-
+const CompletedList = ({}) => {
+  const { todoList } = useContext(TodoContext);
+  const completedList = todoList.filter((item) => item.isCompleted);
+  const renderTodoItem = (item: Todo) => {
+    return <TodoItem item={item} key={item.id} />;
+  };
   return (
     <article>
       <h3>완료</h3>
       <div className={styles.todoListWrap}>
-        {dummy.map((item) => {
-          return <TodoItem item={item} />;
-        })}
+        {completedList.map(renderTodoItem)}
       </div>
     </article>
   );
