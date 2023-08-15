5324ab80578d75595a00f71a54abd1f257cf019c
hoinlee-moi <lhi3048@gmail.com>
2023-08-15 13:29:31 +0900

    feat : create error page

--- src/pages/NoMatch.tsx
+++ src/pages/NoMatch.tsx
@@ -1,6 +1,17 @@
-
+import { useNavigate } from "react-router";
+import Button from "../components/ui/Button";
+import styles from "../styles/pages/noMatch.module.css";
 const NoMatch = () => {
-    return <div>오류 페이지입니다</div>
-}
+  const navigation = useNavigate();
 
-export default NoMatch
+  const redirectClickHandler = () => navigation("/");
+  return (
+    <div className={styles.pageContainer}>
+      <h1>404 not found</h1>
+      <p>올바른 경로로 접속해주세요!</p>
+      <Button clickHandler={redirectClickHandler}>홈으로 돌아가기</Button>
+    </div>
+  );
+};
+
+export default NoMatch;
