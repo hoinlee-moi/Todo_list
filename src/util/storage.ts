67de81ced8925c30c4b44f610e6beb815e437430
hoinlee-moi <lhi3048@gmail.com>
2023-08-15 17:51:06 +0900

    feat :logout create

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -1,8 +1,12 @@
 export const setLocalStorage = (key: string, value: string) => {
-    // 과제 상세 내용으로 expire 토큰관리 없으니 제외
-    localStorage.setItem(key,value)
+  // 과제 상세 내용으로 expire 토큰관리 없으니 제외
+  localStorage.setItem(key, value);
 };
 
-export const getLocalStorage = (key:string):string | null => {
-    return localStorage.getItem(key)
-}
+export const getLocalStorage = (key: string): string | null => {
+  return localStorage.getItem(key);
+};
+
+export const deleteLocalStorage = (key: string): void => {
+  localStorage.removeItem(key);
+};
